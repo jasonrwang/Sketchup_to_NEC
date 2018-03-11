838157a2cc565ee02038c547f15f389c80ef2c0f
Jason R. Wang <accounts@jrwang.ca>
2018-03-11 15:28:54 -0600

    20180311.002
    Syntax fix to be compatible with cocoaNEC 2.0:
    * The previous file was incompatible with the NEC card deck format that cocoaNEC 2.0 accepted. This version fixes it.

--- to_nec.rb
+++ to_nec.rb
@@ -4,24 +4,20 @@ Title:
     
 Description:
     Convert edges (lines) to GW (wires) cards in Meters.
-
 Version:
-    20160818.001
+    20180311.002
 Author:
-    EllisGL
-
+    EllisGL and jasonrwang
 Installation:
     Save this file to your Sketchup plugin folder as "to_nec.rb"
     See https://www.sketchup.com/intl/en/developer/docs/loading for more info.
-
 Usage:
     Make sure that all groups and components are exploded before running this plugin.
     Click "Extensions", then "Convert to NEC".
     The console window will pop up and output the GW lines. Copy to your NEC file.
-
 Change Log:
-    20160818.001
-    Initial release
+    20180311.002
+    jasonrwang update
 =end
 require 'sketchup.rb'
 
@@ -48,15 +44,17 @@ UI.menu("Plugins").add_item("Convert to NEC") {
             # Convert everthing to meters.
             # Then to a floating point with 6 decimal places.
             # Then to a string.
-            sx = ("%0.6f" % vs.x.to_m.to_f).to_s
-            sy = ("%0.6f" % vs.y.to_m.to_f).to_s
-            sz = ("%0.6f" % vs.z.to_m.to_f).to_s
-            ex = ("%0.6f" % ve.x.to_m.to_f).to_s
-            ey = ("%0.6f" % ve.y.to_m.to_f).to_s
-            ez = ("%0.6f" % ve.z.to_m.to_f).to_s
-            
+            l_out = ("%3.f" % (l-999)).to_s
+            sx = ("%10.6f" % vs.x.to_m.to_f).to_s
+            sy = ("%10.6f" % vs.y.to_m.to_f).to_s
+            sz = ("%10.6f" % vs.z.to_m.to_f).to_s
+            ex = ("%10.6f" % ve.x.to_m.to_f).to_s
+            ey = ("%10.6f" % ve.y.to_m.to_f).to_s
+            ez = ("%10.6f" % ve.z.to_m.to_f).to_s
+            r = ("%10f" % input[0].to_f).to_s
+
             # Add line to output variable (printing this out line by line in the loop is super slow)
-            output = output + "GW\t" + l.to_s + "\t" + "1\t" + sx + "\t" + sy + "\t" + sz + "\t" + ex + "\t" + ey + "\t" + ez + "\t" + input[0] + "\n"
+            output = output + "GW" + l_out + "   21" + sx + sy + sz + ex + ey + ez + r + "\n"
 
             #Increment the line counter
             l += 1
